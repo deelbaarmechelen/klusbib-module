6b1e0ee31ab924fa1fda0146234f56becef463a6
Bernard Butaye <bernardenmartine@gmail.com>
2020-04-14 00:11:19 +0200

    remove next id asset tag update as observer is updating it first

--- Http/Controllers/Api/AssetsController.php
+++ Http/Controllers/Api/AssetsController.php
@@ -22,47 +22,25 @@ class AssetsController extends \App\Http\Controllers\Api\AssetsController
      */
     public function customStore(AssetRequest $request)
     {
-        DB::beginTransaction();
-        try {
-            $autoAssetIncrement = false;
-            $asset_tag = $request->get('asset_tag');
-            if (!isset($asset_tag)) {
-                $serial                  = $request->get('serial');
-                $company_id              = Company::getIdForCurrentUser($request->get('company_id'));
-                $companyPrefix = $this->getCompanyPrefix($company_id);
-                // get next value for asset_tag
-                $next_seq_id = 1;
-                $asset_tag_pattern = $companyPrefix . '-' . date('y') . '-' . '000';
-                $assetPattern = AssetTagPattern::query()->where('pattern', '=', $asset_tag_pattern)->lockForUpdate()->first();
-                if (isset($assetPattern)) {
-                    $next_seq_id = $assetPattern->next_auto_id;
-                }
-                $asset_tag = str_replace('000', Asset::zerofill($next_seq_id, 3), $asset_tag_pattern);
-                $request->request->set('asset_tag', $asset_tag);
-                $autoAssetIncrement = true;
+        $asset_tag = $request->get('asset_tag');
+        if (!isset($asset_tag)) {
+            $serial                  = $request->get('serial');
+            $company_id              = Company::getIdForCurrentUser($request->get('company_id'));
+            $companyPrefix = $this->getCompanyPrefix($company_id);
+            // get next value for asset_tag
+            $next_seq_id = 1;
+            $asset_tag_pattern = $companyPrefix . '-' . date('y') . '-' . '000';
+            $assetPattern = AssetTagPattern::query()->where('pattern', '=', $asset_tag_pattern)->first();
+            if (isset($assetPattern)) {
+                $next_seq_id = $assetPattern->next_auto_id;
             }
-
-            $jsonResponse = parent::store($request);
-
-            if ($autoAssetIncrement) {
-                if (isset($assetPattern)) {
-                    $assetPattern->increment('next_auto_id');
-                } else {
-                    $assetPattern = new AssetTagPattern();
-                    $assetPattern->pattern = $asset_tag_pattern;
-                    $assetPattern->next_auto_id = 1;
-                    $assetPattern->save();
-                }
-            }
-            DB::commit();
-
-            return $jsonResponse;
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
+            $asset_tag = str_replace('000', Asset::zerofill($next_seq_id, 3), $asset_tag_pattern);
+            $request->request->set('asset_tag', $asset_tag);
         }
-    }
 
+        return parent::store($request);
+    }
+    
     private function getCompanyPrefix($companyId) {
         $company = Company::find((int) $companyId);
         if (strcasecmp($company->name ,"DIGIBIB") == 0) {
